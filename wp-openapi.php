1b7d38d1192938d32b5e85ad339b0ed3a095e161
moon <moon.kyong@automattic.com>
2024-10-11 10:58:26 -0700

    Bump version to 1.0.15

--- wp-openapi.php
+++ wp-openapi.php
@@ -9,7 +9,7 @@
  *
  * Plugin Name: WP OpenAPI
  * Plugin URI: https://github.com/moon0326/wp-openapi
- * Version:     1.0.14
+ * Version:     1.0.15
  * Author:      Moon K
  * Author URI: https://github.com/moon0326
  * License:     GPL v2 or later
