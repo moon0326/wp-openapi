25b8a2430a55be62aad397e402888ebd38257cce
moon <moon.kyong@automattic.com>
2024-10-15 12:20:44 -0700

    Use get_inline_script_data -- print_inline_script has been deprecated

--- resources/views/export-html.php
+++ resources/views/export-html.php
@@ -4,10 +4,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php echo esc_html($title)?></title>
+	<script>
     <?php
         global $wp_scripts;
+       	echo $wp_scripts->get_inline_script_data('elements-js');
+    ?>
+	</script>
+    <?php
         global $wp_styles;
-        $wp_scripts->print_inline_script('elements-js');
         $wp_styles->print_inline_style('elements-style');
     ?>
 </head>
